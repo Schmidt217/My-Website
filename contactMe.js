6955ba3037d245eeecc1d22de60c171600b14f55
Mike Schmidt <schmidt217@gmail.com>
2021-08-14 14:39:15 -0600

    imported css files
    Only have one css file on each index.html file

--- contactMe.js
+++ contactMe.js
@@ -8,12 +8,6 @@ const submitBtn = document.getElementById("submit-button");
 const openModal = function (e) {
 	e.preventDefault();
 	document.body.style = "overflow: hidden";
-	document.body.style = "position: fixed";
-	// const scrollY = document.body.style.top;
-	// document.body.style.position = "";
-	// document.body.style.top = "";
-	// window.scrollTo(0, parseInt(scrollY || "0") * -1);
-	// document.body.style.top = `-${window.scrollY}px`;
 	modal.classList.remove("hidden");
 	overlay.classList.remove("hidden");
 	contactName.focus();
