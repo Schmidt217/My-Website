33cd5ebccb2a489515c809437155e41399030705
Mike Schmidt <schmidt217@gmail.com>
2021-09-23 12:32:53 -0600

    Back to original

--- netlify/functions/send-email/index.js
+++ netlify/functions/send-email/index.js
@@ -1,20 +1,20 @@
 const sgMail = require("@sendgrid/mail");
-const dotenv = require("dotenv");
-dotenv.config({ path: "../../../.env" });
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "../../../.env" });
 
-console.log(process.env);
+// console.log(process.env);
 
-const api_key = process.env.API_KEY;
+// const api_key = process.env.API_KEY;
 
-// const A = [
-// 	"SG",
-// 	"QMkoHwckT9WaiuiN4LsZjw",
-// 	"Xr2UDzCeHumsIRRZKmtQ3c8WlB9WqetUAjqjBOvhoM0",
-// ];
+const A = [
+	"SG",
+	"QMkoHwckT9WaiuiN4LsZjw",
+	"Xr2UDzCeHumsIRRZKmtQ3c8WlB9WqetUAjqjBOvhoM0",
+];
 
-// const API_KEY = A.join(".");
+const API_KEY = A.join(".");
 
-sgMail.setApiKey(api_key);
+sgMail.setApiKey(API_KEY);
 
 exports.handler = async function (event, context) {
 	try {
